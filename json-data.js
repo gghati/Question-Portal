09e93e4575ae909d7bc340dfa0a82745d0650c08
gauravghati <gauravghati225@gmail.com>
2020-07-21 23:56:20 +0530

    init push

---
+++ json-data.js
@@ -0,0 +1,16 @@
+const members = [
+    {
+        "id": 1,
+        "name": "Gaurav",
+    },
+    {
+        "id": 2,
+        "name": "Gaurav",
+    },
+    {
+        "id": 3,
+        "name": "Gaurav",
+    },
+];
+
+module.exports = members;
